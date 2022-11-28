48556b629d978c63774a42e7ddbb9a7c3193b8f5
Sebastian Bjelkstam <sebastian.bjelkstam@gmail.com>
2022-11-28 16:08:58 +0100

    Merge

---
+++ src/ts/main.ts
@@ -0,0 +1,37 @@
+class Person {
+  namn: string;
+  constructor(namn: string) {
+    this.namn = namn;
+  }
+}
+
+let p = new Person("Hej");
+
+let p1 = new Person("Tja");
+
+let p2 = new Person("Hejsan");
+
+let p3 = new Person("Tjaba");
+
+let p4 = new Person("Tjabaaaaa");
+
+let names = 0;
+
+let myuls = [p, p1, p2, p3, p4];
+
+let ulTag = document.getElementById("myUL");
+
+for (let i = 0; i < myuls.length; i++) {
+  let li = document.createElement("li");
+
+  let span = document.createElement("span");
+
+  var txt = document.createTextNode("\u00D7");
+
+  let ull = document.createElement("button");
+  li.innerHTML = myuls[i].namn;
+
+  li.appendChild(span);
+  ull.appendChild(span);
+  ulTag?.appendChild(li);
+}
